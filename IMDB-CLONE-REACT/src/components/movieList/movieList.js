efcedafc5d55ddc7db8f72e3e0362c8a442967c5
Raman Prasad Sharma <167922410+Raman7273@users.noreply.github.com>
2024-05-21 19:03:43 +0530

    Add files via upload
    FIRST COMMIT

---
+++ IMDB-CLONE-REACT/src/components/movieList/movieList.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Cards from "../card/card";
+import "./movieList.css";
+import { useParams } from "react-router-dom";
+
+const MovieList = () => {
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [type]);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=868b408d0f6a987b2e4c30be13d8489b&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  };
+
+  return (
+    <div className="movie__list">
+      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+      <div className="list__cards">
+        {movieList.map((movie) => (
+          <Cards movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
